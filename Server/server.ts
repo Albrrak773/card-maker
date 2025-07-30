3b803ea48a4c6c6a83c8a53f497ec7af473dfb1c
albrrak <albrrak773@gmail.com>
2025-07-30 22:55:21 +0300

    deleted the js file now that I figured out how to get the ts compiler to behave
    * moved the code from the *.js file to the typescript file
    * updated the tsconfig to ignore node_modules
    * updated package.json to not pass the file to tsc

--- Server/server.ts
+++ Server/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { spawn } from "node:child_process";
-import type { Request, Response } from "express";
+import { exec } from "node:child_process";
+import { Request, Response } from "express";
 
 const app = express()
 const PORT = 8080
@@ -9,15 +9,22 @@ app.get("/:name", handleName)
 
 
 async function handleName(req:Request, res:Response) {
-    const name = req.params.name;
-    const ls = spawn('ls')
-    const image = spawn(`python3`, ["main.py", name]);
-
-    res.type("png");
-    res.status(200);
-    res.sendFile("./output.png")
     
-
-    console.log(ls);
-
+    const name = req.params.name.replace("+", " ");
+    const cwd = process.cwd() 
+    const cmd = `'${cwd + "/Scripts/.pyenv/bin/python3"}' '${cwd + "/Scripts/main.py"}' '${name}' --stdout`
+    const bufferSize = 1024 * 1024 * 10 // 1MB 
+    exec(cmd, {"maxBuffer": bufferSize, encoding: null },  (err, stdout, stderr) => {
+        if (err){
+            console.log(err.code);
+            console.log(err.message);
+        }
+        res.type("png");
+        res.status(200);
+        res.send(stdout);
+    });
 }
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}...`);
+})
